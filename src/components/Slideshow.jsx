28af383364393c903b254e8d2bf521c85f27f374
Chad <portableanswers@gmail.com>
2022-03-25 15:43:34 -0700

    Slideshow functionality working. Pictures display starting from beginning and loop back around once the end is reached upon pressing the next-arrow

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -2,22 +2,44 @@ import React, { useState } from "react";
 import "../styles/slideshow.css";
 import { SlideshowData } from "./SlideshowData";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+import uuid from "react-uuid";
 
 const Slideshow = ({ slides }) => {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
 
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  console.log(current);
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null; //if no data in SlideshowData then return null
+  }
+
   return (
     <section className="slideshow">
-      <FaArrowAltCircleLeft className="arrow-left" />
-      <FaArrowAltCircleRight className="arrow-right" />
+      <FaArrowAltCircleLeft className="arrow-left" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="arrow-right" onClick={nextSlide} />
       {SlideshowData.map((chadsThisItem, index) => {
         return (
-          <img
-            src={chadsThisItem.image}
-            alt="grilled cheese"
-            className="image"
-          />
+          <div
+            className={index === current ? "slide-active" : "slide"}
+            key={uuid()}
+          >
+            {index === current && (
+              <img
+                src={chadsThisItem.image}
+                alt="grilled cheese"
+                className="image"
+              />
+            )}
+          </div>
         );
       })}
     </section>
