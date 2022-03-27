621e28c775bf5043e692d60e20dcf68e9d367dd2
Chad <portableanswers@gmail.com>
2022-03-27 13:03:21 -0700

    Added Board and Card component for Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "../styles/game.css";
+import Board from "./Board";
+import Card from "./Card";
 
 const Game = () => {
   return (
-    <>
-      <main>
-        <h2>Welcome to the game!</h2>
+    <div className="game">
+      <main className="flexbox">
+        <Board id="board-1" className="board">
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card One</p>
+          </Card>
+        </Board>
+
+        <Board id="board-2" className="board">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card Two</p>
+          </Card>
+        </Board>
       </main>
-    </>
+    </div>
   );
 };
 
