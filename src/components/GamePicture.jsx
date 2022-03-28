59f38d594adc5f21a92a58c40ca386c87340257a
Chad <portableanswers@gmail.com>
2022-03-27 17:45:26 -0700

    Added a border that tests when an element is being dragged

--- src/components/GamePicture.jsx
+++ src/components/GamePicture.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 
 function GamePicture({ id, image }) {
-  return <img id={id} className="game-pics" src={image} alt="game" />;
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "image",
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+  return (
+    <img
+      id={id}
+      className="game-pics"
+      src={image}
+      alt="game"
+      style={{ border: isDragging ? "5px solid pink" : "0px" }}
+      ref={drag}
+    />
+  );
 }
 
 export default GamePicture;
