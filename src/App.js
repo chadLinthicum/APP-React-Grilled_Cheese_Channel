a77c64ee231baacee5905e8296b9669c772faf57
Chad <portableanswers@gmail.com>
2022-03-28 14:51:58 -0700

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/home" exact element={<Home />} />
         <Route path="/checklist" exact element={<Checklist />} />
         <Route
           path="/slideshow"
